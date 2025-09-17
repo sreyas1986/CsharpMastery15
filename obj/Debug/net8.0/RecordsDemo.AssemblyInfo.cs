4d186c2c36e9f224c3357c0953d6b42e1a4e4769
sreyas <31040811+sreyascv@users.noreply.github.com>
2025-09-17 07:39:01 +0530

    Second day Longestsubstring

--- obj/Debug/net8.0/RecordsDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/RecordsDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecordsDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4540ef9cb18448f8aed807d6475026a7789af21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f70034bee860d01006197dd2d2e6677f13f9db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecordsDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecordsDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
