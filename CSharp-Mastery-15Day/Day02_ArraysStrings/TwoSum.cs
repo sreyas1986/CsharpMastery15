9048f6b46896f9e7da23da73916d826074ba2502
sreyas <31040811+sreyascv@users.noreply.github.com>
2025-09-18 06:34:18 +0530

    2 sum pattern

--- CSharp-Mastery-15Day/Day02_ArraysStrings/TwoSum.cs
+++ CSharp-Mastery-15Day/Day02_ArraysStrings/TwoSum.cs
@@ -1,2 +1,39 @@
 // TwoSum.cs
 // Two Sum problem solution
+using System;
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+   Dictionary<int,int> result= new Dictionary<int,int>();
+
+  for(int i=0;i<nums.Length;i++){
+   int compliment = target-nums[i];
+   if(result.ContainsKey(compliment)){
+    return new int[] {result[compliment],i};
+   }
+   result[nums[i]]=i;
+
+  }
+  return new int[0];
+
+    }
+     public int[] TwoSum_Easyoption(int[] nums, int target) {
+        int[] output= new int[2];
+        for(int i=0;i<nums.Length-1;i++){
+
+            for(int j=i+1;j<nums.Length;j++){
+                if(nums[i]+nums[j]==target){
+
+                        output[0]=i;
+                        output[1]=j;
+                        break;
+                }
+            }
+            if(output[1]>0){
+                break;
+
+            }
+            
+        }
+        return output;
+    }
+}
